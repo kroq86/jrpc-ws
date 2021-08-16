6ef6237b0117a3374f52a3355a7531926b3a1150
Kirill Ostapenko <djkroq@gmail.com>
2021-08-16 18:32:13 +0500

    Update ws.py
    NO jrpc

--- ws.py
+++ ws.py
@@ -1,15 +1,10 @@
 import asyncio
 import websockets
 from websockets import WebSocketServerProtocol
-from jsonrpcserver import method, Success, Result
 
 
 class Server:
     clients = set()
-
-    @method
-    async def sendEcho() -> Result:
-        return Success("Message for me")
 
     async def register(self, ws:WebSocketServerProtocol) -> None:
 	    self.clients.add(ws)
@@ -17,11 +12,14 @@ class Server:
     async def unregister(self, ws:WebSocketServerProtocol) -> None:
 	    self.clients.remove(ws)
 
-    @method
-    async def sendMessage(self, message: str) -> Result:
+    async def sendMessage(self, message: str) -> None:
+        message = "Hello to all"
         if self.clients:
             await asyncio.wait([client.send(message) for client in self.clients])
-        return Success("Hello to all")
+
+    async def distribute(self, ws: WebSocketServerProtocol) -> None:
+        async for message in ws:
+            await self.sendMessage(message)
 
     async def ws_handler(self, ws: WebSocketServerProtocol, uri: str) -> None:
 	    await self.register(ws)
@@ -30,14 +28,9 @@ class Server:
 	    finally: 
                 await self.unregister(ws)
 
-    async def distribute(self, ws: WebSocketServerProtocol) -> None:
-        async for message in ws:
-            await self.sendMessage(message)
 
 
 server = Server()
 start_server = websockets.serve(server.ws_handler,'localhost',5000)
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
-
-
