de122f70d6f54ecf6d0c933772ee4e13b9196c22
Kirill Ostapenko <djkroq@gmail.com>
2021-08-18 17:39:00 +0500

    Create ws2.py

---
+++ ws2.py
@@ -0,0 +1,65 @@
+import asyncio
+import logging
+import websockets
+from websockets import WebSocketServerProtocol
+import time
+import threading
+from jsonrpcserver import method, Success, Result, async_dispatch
+
+
+logging.basicConfig(level=logging.INFO)
+
+
+class Server:
+
+    clients = set()
+        
+    async def register(self, ws: WebSocketServerProtocol) -> None:
+        self.clients.add(ws)
+        logging.info("connects")
+
+    async def unregister(self, ws: WebSocketServerProtocol) -> None:
+        self.clients.remove(ws)
+        logging.info("disconnects")
+
+    async def send_to_clients(self, message: str) -> None:
+        if self.clients:
+            logging.info("send")
+            await asyncio.wait([client.send(message) for client in self.clients])
+
+    async def ws_handler(self, ws: WebSocketServerProtocol, url: str) -> None:
+        await self.register(ws)
+        try:
+            await self.distribute(ws)
+        finally:
+            await self.unregister(ws)
+
+    async def distribute(self, ws: WebSocketServerProtocol) -> None:
+        for message in ws:
+            self.send_to_clients(message)
+            logging.info("send")
+
+
+server = Server()
+
+
+@method
+async def sendEcho() -> Result:
+    return Success("Echo Message")            
+
+
+@method
+async def checkAndSend() -> Result:
+    await server.ws_handler('localhost',5000)
+    return Success("Hi there")  
+
+
+async def main(websocket, path):
+    if response := await async_dispatch(await websocket.recv()):
+        await websocket.send(response)
+
+
+start_server = websockets.serve(main,'localhost',5000)
+loop = asyncio.get_event_loop()
+loop.run_until_complete(start_server)
+loop.run_forever()
